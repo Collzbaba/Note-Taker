800cb08d751c3b5b02a38804fcfeb3a80049cc4a
Blessed Adodo <collzbadodo@gmail.com>
2022-09-01 21:48:24 -0700

    created the GET route for api/notes

--- Develop/Develop/Main/server.js
+++ Develop/Develop/Main/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
@@ -17,6 +19,13 @@ console.log("hi i made it")
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 }
 
+);
+
+app.get('/api/notes', (req, res) => {
+  console.log("hi i made it")
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  console.log (res); 
+  }
 );
 
 app.listen(PORT, () =>
