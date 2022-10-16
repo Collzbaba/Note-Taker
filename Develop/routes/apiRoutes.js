6bf3480e097cdbe0c2e2edb853d72610cc1c6cce
Blessed Adodo <collzbadodo@gmail.com>
2022-10-15 18:38:59 -0700

    modified images folder by adding screenshots of application

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,52 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
+// fsUtil
+const readFromFile = util.promisify(fs.readFile);
+const writeToFile = util.promisify(fs.writeFile);
 
+// GET api request route
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+  readFromFile('./db/db.json', 'utf8').then(function(data) {
+    const notes = [].concat(JSON.parse(data))
+    res.json(notes);
+  })
+})
+
+// POST api request route
+router.post('/', (req, res) => {
+  const note = req.body;
+  readFromFile('./db/db.json').then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    note.id = notes.length + 1
+    notes.push(note);
+    return notes;
+  })
+  .then(function(notes) {
+    writeToFile('./db/db.json', JSON.stringify(notes))
+    res.json(note);
+  })
 });
 
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'))
-});
-
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-});
+// DELETE api request route
+router.delete('/:id', (req, res) => {
+  const idToDelete = parseInt(req.params.id);
+  readFromFile('./db/db.json','utf8').then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    const newNoteData = [];
+    for (let i = 0; i < notes.length; i++) {
+      if(idToDelete !== notes[i].id) {
+        newNoteData.push(notes[i])
+      }
+    }
+    return newNoteData;
+  })
+  .then(function(notes) {
+    writeToFile('./db/db.json', JSON.stringify(notes))
+    res.send('saved success!')
+  }) 
+})
 
 module.exports = router;
