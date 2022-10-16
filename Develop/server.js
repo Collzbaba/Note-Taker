dccbabe8bbd6abbe580b06b204d7fbae1762b17a
Blessed Adodo <collzbadodo@gmail.com>
2022-10-15 18:27:20 -0700

    created routes folder, added apiRoutes, homeRoutes files and modified server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,50 +1,23 @@
 const express = require('express');
 const path = require('path');
 
+const apiRoutes = require("./routes/apiRoutes")
+const homeRoutes = require("./routes/homeRoutes")
+
+
+const PORT = process.env.PORT || 3001;
+
 const app = express();
-const PORT = 3001;
 
-const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.use("/api/notes", apiRoutes);
+app.use("/", homeRoutes);
 
-app.get('/send', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
-);
 
-app.get('/notes', (req, res) => {
-console.log("hi i made it")
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-}
-
-);
-
-app.get('/api/notes', (req, res) => {
-  console.log("hi i made it")
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  // console.log (res); 
-  }
-);
-app.post('/api/notes', (req, res) => {
-  // console.info(`${req.method}`);
-  console.log(`${req}`);
-
-  const { NoteTitle, NoteText,  } = req;
-  if (req.body) {
-    const newNote = {
-      NoteTitle,
-      NoteText,
-    };
-
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully 🚀`);
-  } else {
-    res.error('Error in adding note');
-  }
-});
-
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`App listening on port  http://localhost:${PORT}`);
+})
